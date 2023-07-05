ea86f9bf32e150d72424868b65b43be0bda45fc6
ugbeadie <ugbeadie3@gmail.com>
2023-07-05 22:30:33 +0100

    render image

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,27 @@
 import * as THREE from "three";
 
+//scene
 const scene = new THREE.Scene();
 
+//create our sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64);
+
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+//camera
+
+const camera = new THREE.PerspectiveCamera(45, 800, 600);
+
+scene.add(camera);
+
+//renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
